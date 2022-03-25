991af3f029646904fe41a8daaae0f64419f492d4
Gonzalo Garro <gonzalo.garro@patagonian.it>
2022-03-25 12:36:20 -0300

    Remove unneded code

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 Firebase credentials to connect to Firestore. Create an android app from the Firebase
 [console](https://console.firebase.google.com/) then copy the `google-services.json` file to `/app`
 
+## How to use the app
+- Press `Create Offer` button to create a connection hash
+- Use the [web app](https://github.com/kocholes/webrtc-web-test) as the other peer, paste the 
+  generated connection hash an press `Connect`. Both peers must be on the same local network
+- If the connection is successful:
+  - Write a message and press `Send` to send it to the other peer. 
+  - Press `Ping` to calculate the transmission time from one peer to another
+  - Press `Send Roll` to start sending rotation data in realtime to the other peer
--- app/src/main/java/com/ggarro/kotlin/orientation/RotationVectorManager.kt
+++ app/src/main/java/com/ggarro/kotlin/orientation/RotationVectorManager.kt
@@ -8,20 +8,15 @@ import android.hardware.SensorManager
 class RotationVectorManager : BaseOrientationManager {
 
     constructor(context: Context) : super(context) {
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR)
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
     }
 
     override fun getOrientation(event: SensorEvent): String? {
         if (event.sensor.type == sensor?.type) {
             val rotationMatrix = FloatArray(16)
-            val rotatedRotationMatrix = FloatArray(16)
             val orientation = FloatArray(3)
             SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
-//            SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Y, rotatedRotationMatrix)
             SensorManager.getOrientation(rotationMatrix, orientation)
-//            val yaw = (orientation[0] * 180 / Math.PI).toInt() + 90
-//            val pitch = (-orientation[2] * 180 / Math.PI).toInt() - 90
-//            val roll = (-orientation[1] * 180 / Math.PI).toInt()
             val yaw = 0//orientation[0]
             val pitch = orientation[1]
             val roll = orientation[2]
